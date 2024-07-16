4905acc1eb0d76556769fc3fd9714487ce370a25
Ademilua Adeola Israel <ademiluaadeola969@gmail.com>
2024-07-16 07:56:32 +0100

    updated

--- webinar/settings.py
+++ webinar/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
